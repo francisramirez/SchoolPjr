f1ec2c9f1c8ec86dac5d79e69cb6855c6e4b85b3
Francis Ramirez <Francis Ramirez@LAPTOP-DF01N7UN>
2023-09-09 16:15:35 -0400

    ok

--- SchoolPjr.DAL/Core/BaseEntity.cs
+++ SchoolPjr.DAL/Core/BaseEntity.cs
@@ -6,6 +6,11 @@ namespace SchoolPjr.DAL.Core
 {
     public abstract class BaseEntity
     {
+        public BaseEntity()
+        {
+            this.CreationDate = DateTime.Now;
+            this.CreationUser = 1;
+        }
         public DateTime CreationDate { get; set; }
         public DateTime? ModifyDate { get; set; }
         public int CreationUser { get; set; }
--- SchoolPjr.DAL/Entities/StudentGrade.cs
+++ SchoolPjr.DAL/Entities/StudentGrade.cs
@@ -1,8 +1,12 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace SchoolPjr.DAL.Entities
 {
     public class StudentGrade
     {
+
+        [Key]
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
